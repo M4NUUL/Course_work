b9379ba8a4571ac43cfd6e08f14f40e8793dbd9a
m4nuul <nikitakortrlr020@gmail.com>
2025-12-22 18:41:46 +0700

    Проект начат с 0. В gitignore добавлена папка build.

--- run.sh
+++
@@ -1,5 +0,0 @@
-set -euo pipefail
-XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
-env -i HOME="$HOME" USER="$USER" PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
-DISPLAY="${DISPLAY:-}" XAUTHORITY="${XAUTHORITY:-}" XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR" \
-./build/CourseWorkApp
